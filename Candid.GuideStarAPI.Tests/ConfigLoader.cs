a5b94c83b1ccda9723f72957470127276f3ea52c
David P <david.patterson@candid.org>
2020-06-08 15:28:33 -0400

    brought test back into the sln and it works without the nuget package (#6)
    * brought test back into the sln and it works without the nuget package
    
    * removed interfaces that were added back erroneously

---
+++ Candid.GuideStarAPI.Tests/ConfigLoader.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Candid.GuideStarApiTest
+{
+  public static class ConfigLoader
+  {
+    public static IConfiguration InitConfiguration()
+    {
+      var config = new ConfigurationBuilder()
+          .AddJsonFile("appsettings.test.json")
+          .AddUserSecrets<Keys>()
+          .Build();
+      return config;
+    }
+  }
+}
---
+++ Candid.GuideStarAPI.Tests/Keys.cs
@@ -0,0 +1,10 @@
+﻿namespace Candid.GuideStarApiTest
+{
+  class Keys
+  {
+    public string CHARITY_CHECK_KEY { get; set; }
+    public string CHARITY_CHECK_PDF_KEY { get; set; }
+    public string ESSENTIALS_KEY { get; set; }
+    public string PREMIER_KEY { get; set; }
+  }
+}
