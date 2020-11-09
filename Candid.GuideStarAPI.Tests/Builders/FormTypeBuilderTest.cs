b2b2bcfb008665ef1b2f1480edc0808c0a7861cc
David Patterson <david.patterson@candid.org>
2020-11-09 08:55:03 -0500

    added an integration test for builder

--- Candid.GuideStarAPI.Tests/Builders/FormTypeBuilderTest.cs
+++ Candid.GuideStarAPI.Tests/Builders/FormTypeBuilderTest.cs
@@ -12,16 +12,12 @@ namespace Candid.GuideStarAPI.Tests.Builders
   public class FormTypeBuilderTest
   {
     private readonly IConfiguration _config;
-    private static string CHARITY_CHECK_KEY;
     private static string ESSENTIALS_KEY;
-    private static string PREMIER_KEY;
 
     public FormTypeBuilderTest()
     {
       _config = ConfigLoader.InitConfiguration();
-      CHARITY_CHECK_KEY = _config["Keys:CHARITY_CHECK_KEY"];
       ESSENTIALS_KEY = _config["Keys:ESSENTIALS_KEY"];
-      PREMIER_KEY = _config["Keys:PREMIER_KEY"];
 
       SetSubscriptionKeys();
     }
@@ -31,14 +27,8 @@ namespace Candid.GuideStarAPI.Tests.Builders
       // Only do this once
       if (!GuideStarClient.SubscriptionKeys.IsEmpty())
         return;
-      if (!string.IsNullOrEmpty(CHARITY_CHECK_KEY))
-        GuideStarClient.SubscriptionKeys.Add(Domain.CharityCheckV1, CHARITY_CHECK_KEY);
       if (!string.IsNullOrEmpty(ESSENTIALS_KEY))
         GuideStarClient.SubscriptionKeys.Add(Domain.EssentialsV2, ESSENTIALS_KEY);
-      if (!string.IsNullOrEmpty(PREMIER_KEY))
-        GuideStarClient.SubscriptionKeys.Add(Domain.PremierV3, PREMIER_KEY);
-      if (!string.IsNullOrEmpty(ESSENTIALS_KEY))
-        GuideStarClient.SubscriptionKeys.Add(Domain.Lookup, ESSENTIALS_KEY);
     }
     private static void TestPayload(SearchPayload payload)
     {
